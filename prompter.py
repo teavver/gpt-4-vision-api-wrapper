2a1bff309cca1106036fb013118985293c09b2db
teavver <jerzyx11@gmail.com>
2023-10-23 01:09:15 +0200

    fix first time setup

--- prompter.py
+++ prompter.py
@@ -42,6 +42,8 @@ class Prompter:
         continue_btn = self.find_elem_with_timeout(By.CLASS_NAME, "_button-login-password")
         if continue_btn:
             continue_btn.click()
+
+        time.sleep(1.0)
         cookies.save_cookies(self.driver)
         return
     
@@ -53,27 +55,23 @@ class Prompter:
         if not self.cookies_present:
             print("[prompter] no cookies file found. initializing first time login")
             self.init_cookies()
-            time.sleep(2.5)
         else:
             cookies.load_cookies(self.driver)
-            time.sleep(1)
+            self.driver.refresh()
 
-        self.driver.refresh()
         self.driver.get(config.OPENAI_GPT4_URL)
-
-
 
         # close the welcome pop-up
         welcome_popup = self.find_elem_with_timeout(By.XPATH, '//button[.//div[text()="Okay, let’s go"]]')
         if welcome_popup:
             welcome_popup.click()
-            time.sleep(1)
+            time.sleep(0.5)
 
-        # # # and the second one
-        # search_with_imgs_popup = self.find_elem_with_timeout(By.XPATH, "//*[contains(@class, '-my-1') and contains(@class, '-mr-1') and contains(@class, 'p-1') and contains(@class, 'opacity-70')]")
-        # if search_with_imgs_popup:
-        #     search_with_imgs_popup.click()
-        #     time.sleep(1)
+        # and the second one
+        search_with_imgs_popup = self.find_elem_with_timeout(By.XPATH, "//*[contains(@class, '-my-1') and contains(@class, '-mr-1') and contains(@class, 'p-1') and contains(@class, 'opacity-70')]")
+        if search_with_imgs_popup:
+            search_with_imgs_popup.click()
+            time.sleep(0.5)
 
         # file_inputs = self.driver.find_elements(By.XPATH, '//input[@type="file"]')
         # if len(file_inputs) > 0:
@@ -84,9 +82,8 @@ class Prompter:
         # prompt_textarea = self.find_elem_with_timeout(By.ID, "prompt-textarea")
         # if prompt_textarea:
         #     prompt_textarea.click()
-        #     time.sleep(0.5)
         #     prompt_textarea.send_keys(prompt)
-        #     time.sleep(2.5)
-        #     prompt_textarea.send_keys(Keys.RETURN)
+            # time.sleep(2.5)
+            # prompt_textarea.send_keys(Keys.RETURN)
 
         time.sleep(1000)
