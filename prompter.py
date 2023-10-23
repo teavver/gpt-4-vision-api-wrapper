e9662c0ad8e360dee757f40ec6684c6565fc0f32
teavver <jerzyx11@gmail.com>
2023-10-23 02:39:11 +0200

    capture response

--- prompter.py
+++ prompter.py
@@ -1,10 +1,28 @@
 from selenium.webdriver.remote.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 import config, time, cookies, os.path
+
+class GPTResponse:
+    def __init__(self, locator):
+        self.locator = locator
+        self.last_known_text = None
+        self.stable_count = 0
+
+    def __call__(self, driver):
+        element = driver.find_element(*self.locator)
+        if self.last_known_text == element.text:
+            self.stable_count += 1
+        else:
+            self.stable_count = 0
+            self.last_known_text = element.text
+
+        if self.stable_count > 3:
+            return element
+        return False
 
 class Prompter:
 
@@ -12,7 +30,7 @@ class Prompter:
         self.driver = driver
         self.cookies_present = cookies.check_cookies()
 
-    def find_elem_with_timeout(self, by:By, selector:str, max_timeout:int = 120):
+    def find_elem_with_timeout(self, by:By, selector:str, max_timeout:int = 30):
         try:
             element = WebDriverWait(self.driver, max_timeout).until(
                 EC.visibility_of_element_located((by, selector))
@@ -32,12 +50,12 @@ class Prompter:
         if login_input:
             login_input.send_keys(config.OPENAI_LOGIN)
             login_input.send_keys(Keys.RETURN)
-            time.sleep(0.25)
+            # time.sleep(0.25)
         
         pwd_input = self.find_elem_with_timeout(By.ID, "password")
         if pwd_input:
             pwd_input.send_keys(config.OPENAI_PWD)
-            time.sleep(0.25)
+            # time.sleep(0.25)
 
         continue_btn = self.find_elem_with_timeout(By.CLASS_NAME, "_button-login-password")
         if continue_btn:
@@ -47,10 +65,9 @@ class Prompter:
         cookies.save_cookies(self.driver)
         return
     
-    # img path is relative to current working directory (78)
     def vision_prompt(self, img_path:str, prompt:str):
         self.driver.get(config.OPENAI_LOGIN_URL)
-        time.sleep(0.25)
+        # time.sleep(0.25)
         
         if not self.cookies_present:
             print("[prompter] no cookies file found. initializing first time login")
@@ -65,25 +82,43 @@ class Prompter:
         welcome_popup = self.find_elem_with_timeout(By.XPATH, '//button[.//div[text()="Okay, let’s go"]]')
         if welcome_popup:
             welcome_popup.click()
-            time.sleep(0.5)
+            time.sleep(0.25)
 
         # and the second one
         search_with_imgs_popup = self.find_elem_with_timeout(By.XPATH, "//*[contains(@class, '-my-1') and contains(@class, '-mr-1') and contains(@class, 'p-1') and contains(@class, 'opacity-70')]")
         if search_with_imgs_popup:
             search_with_imgs_popup.click()
-            time.sleep(0.5)
+            time.sleep(0.25)
 
-        # file_inputs = self.driver.find_elements(By.XPATH, '//input[@type="file"]')
-        # if len(file_inputs) > 0:
-        #     full_img_path = os.path.join(os.getcwd() + "/" + img_path)
-        #     file_inputs[0].send_keys(full_img_path)
-        #     time.sleep(0.5)
+        file_inputs = self.driver.find_elements(By.XPATH, '//input[@type="file"]')
+        if len(file_inputs) > 0:
+            full_img_path = os.path.join(os.getcwd() + "/" + img_path)
+            file_inputs[0].send_keys(full_img_path)
+            time.sleep(0.25)
             
-        # prompt_textarea = self.find_elem_with_timeout(By.ID, "prompt-textarea")
-        # if prompt_textarea:
-        #     prompt_textarea.click()
-        #     prompt_textarea.send_keys(prompt)
-            # time.sleep(2.5)
-            # prompt_textarea.send_keys(Keys.RETURN)
+        prompt_textarea = self.find_elem_with_timeout(By.ID, "prompt-textarea")
+        if prompt_textarea:
+            prompt_textarea.click()
+            prompt_textarea.send_keys(prompt)
+
+        send_btn_locator = (By.CSS_SELECTOR, 'button[data-testid="send-button"]')
+        try:
+            send_btn = WebDriverWait(self.driver, 45).until(
+                EC.element_to_be_clickable(send_btn_locator)
+            )
+            send_btn.click()
+        except TimeoutException:
+            print("[prompter] timed out waiting for send btn")
+
+        # capture the response
+        response_timeout = 180
+        response_locator = (By.CSS_SELECTOR, '.markdown.prose')
+        try:
+            response = WebDriverWait(self.driver, response_timeout).until(
+                GPTResponse(response_locator)
+            )
+            print(response.text)
+        except TimeoutException:
+            print("[prompter] timed out waiting for response")
 
         time.sleep(1000)
