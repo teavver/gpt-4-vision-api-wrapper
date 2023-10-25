5cbe91b7447168c6719b17d79d15cb297da57d49
teavver <jerzyx11@gmail.com>
2023-10-25 14:56:40 +0200

    fix installer

--- installer.py
+++ installer.py
@@ -1,11 +1,18 @@
 import subprocess
 
 def install_requirements():
+    try:
+        subprocess.run(["python", "-m", "pip", "install", "--upgrade", "pip", "setuptools"], check=True)
+    except Exception as e:
+        print(f"Error updating pip and setuptools: {e}")
+        return
     with open("requirements.txt", "r") as f:
-        packages = f.read().splitlines()
-    
+        packages = f.read().strip().splitlines()
     for package in packages:
-        subprocess.check_call(["pip", "install", package])
+        try:
+            subprocess.run(["python", "-m", "pip", "install", package], check=True)
+        except Exception as e:
+            print(f"Error installing {package}: {e}")
 
 if __name__ == "__main__":
     install_requirements()
